4d1e863e904833b5670a8ecd9b233af057d9690c
Beatriz Yoshimi <yoshimidev1@gmail.com>
2024-12-18 01:07:22 -0300

    primeira tentativa

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,13 @@ import './styles.css'
 import NavBar from '../NavBar';
 import logo from '../../assets/img/logo2.png'
 
+
 export function Header(){
     return (
         <div>
         <header>
             <img id="logo" src={logo} alt="" />
-            <h1>Padaria Sweet Dreams</h1>
+            <h1>Confeitaria Sweet Dreams</h1>
         </header>
         <NavBar />
         </div>
--- src/pages/Galeria/index.jsx
+++ src/pages/Galeria/index.jsx
@@ -1,16 +1,57 @@
-import "../../components/Modal/index.jsx"
-import fotoCabine from '../../assets/Img/cabine.png';
+import React, { useState } from "react";
+import "./styles.css"; // Arquivo CSS para estilização
+import fotoCabine from "../../assets/img/bakery.png";
 
-const Img = () => {
-    return <a href={fotoCabine} data-title="Cabine"> <img className="fotos" src={fotoCabine} alt="" /></a>
-}
+const Lightbox = ({ image, onClose }) => {
+  return (
+    <div className="lightbox-overlay" onClick={onClose}>
+      <div className="lightbox-content" onClick={(e) => e.stopPropagation()}>
+        <img src={image} alt="Preview" className="lightbox-image" />
+        <button className="lightbox-close" onClick={onClose}>
+          ✕
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export function Galeria(){
-    return (
-        <>
-           <Modal/>
-        </>
-    )
-}
+const Galeria = () => {
+  const [isLightboxOpen, setLightboxOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
 
-export default Galeria;
+  const images = [
+    fotoCabine,
+    fotoCabine,
+    fotoCabine
+  ];
+
+  const openLightbox = (image) => {
+    setCurrentImage(image);
+    setLightboxOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setLightboxOpen(false);
+    setCurrentImage(null);
+  };
+
+  return (
+    <div>
+      <h1>Galeria</h1>
+      <div className="gallery">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={` Imagem ${index + 1}` }
+            className="gallery-image"
+            onClick={() => openLightbox(img)}
+          />
+        ))}
+      </div>
+      {isLightboxOpen && <Lightbox image={currentImage} onClose={closeLightbox} />}
+    </div>
+  );
+};
+
+export default Galeria;
